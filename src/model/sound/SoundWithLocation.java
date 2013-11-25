e72cf08dc5e666d4a76d950504883e3a0c0877b0
Chris Cohoon <kiltak@yahoo.com>
2013-11-25 17:40:46 -0500

    Added class that will bundle sound with location.

---
+++ src/model/sound/SoundWithLocation.java
@@ -0,0 +1,25 @@
+/**
+ * This is a way to pass sounds with their locations.  I didn't want to put the location with the
+ * sound because that just felt wrong.  A location doesn't really care where it is.
+ */
+
+package model.sound;
+
+public class SoundWithLocation {
+    
+    public SoundWithLocation(Sound s, Location l) {
+        this.s = s;
+        this.l = l;
+    }
+    
+    public Sound getSound () {
+        return s;
+    }
+    
+    public Location getLocation () {
+        return l;
+    }
+    
+    private final Sound s;
+    private final Location l;
+}
