7d363f745b83750220d206271e6ac494bef3211a
Chris Cohoon <kiltak@yahoo.com>
2014-03-25 15:38:02 -0400

    Made it Java 6 compatible among other things.

--- src/controller/pcg/ChunkHeader.java
+++ src/controller/pcg/ChunkHeader.java
@@ -8,9 +8,9 @@ class ChunkHeader {
     /**
      * 
      * @param data
-     * @throws BadDataException 
+     * @throws BadDataException
      */
-    public int unpack (byte data[], int offset) {
+    public int unpack(byte data[], int offset) {
         assert data.length == BYTE_SIZE;
         
         if (data.length <= offset) {
@@ -18,31 +18,31 @@ class ChunkHeader {
         }
         
         // Chunck ID
-        _chunkId = "";  // Make sure it's cleared out in case of reuse of object
+        _chunkId = ""; // Make sure it's cleared out in case of reuse of object
         for (int i = 0; i < 4; ++i) {
-            _chunkId += (char)data[offset++];
+            _chunkId += (char) data[offset++];
         }
         
-        ByteBuffer wrapped = ByteBuffer.wrap (data);
+        ByteBuffer wrapped = ByteBuffer.wrap(data);
         _size = wrapped.getInt(offset);
         offset += 4;
         
         return offset;
     }
-
+    
     /**
      * 
      * @return
      */
-    public byte[] pack () {
+    public byte[] pack() {
         ByteBuffer data = ByteBuffer.allocate(BYTE_SIZE);
-
+        
         int i;
         
         // chunk ID
         byte[] byteString = _chunkId.getBytes();
         for (i = 0; i < 4; i++) {
-            data.putChar(i, (char)byteString[i]);
+            data.putChar(i, (char) byteString[i]);
         }
         
         data.putInt(4, _size);
@@ -51,59 +51,69 @@ class ChunkHeader {
     }
     
     /**
-     * Get the body's class instantiation.  If there is no body associated with this header type,
-     * return null.
+     * Get the body's class instantiation. If there is no body associated with
+     * this header type, return null.
      * 
      * @return
      * @throws BadDataException
      */
-    public ChunkBody getBodyInstantiation () throws BadDataException {
-        switch (_chunkId) {
-            case "PCG1":
-                return null;
-            case "PRG1":
-                return null;
-            case "PBK1":
-                return new ProgramBankChunk();
-            case "CMB1":
-                return null;
-            case "CBK1":
-                return new CombinationBankChunk();
-            case "DKT1":
-                return null;
-            case "DBK1":
-                return new DrumkitBankChunk();
-            case "ARP1":
-                return null;
-            case "ABK1":
-                return new ArpeggioBankChunk();
-            case "CSM1":
-                return new ChecksumChunk();
-            case "DIV1":
-                return new DividedChunk();
-            case "INI1":
-                return new IniChunk();
-            case "GLB1":
-                return new GlobalChunk();
+    public ChunkBody getBodyInstantiation() throws BadDataException {
+        if ("PCG1".equalsIgnoreCase(_chunkId)) {
+            return null;
         }
-        throw new BadDataException ("Unkown chunk type - " + _chunkId + ".");
+        if ("PRG1".equalsIgnoreCase(_chunkId)) {
+            return null;
+        }
+        if ("PBK1".equalsIgnoreCase(_chunkId)) {
+            return new ProgramBankChunk();
+        }
+        if ("CMB1".equalsIgnoreCase(_chunkId)) {
+            return null;
+        }
+        if ("CBK1".equalsIgnoreCase(_chunkId)) {
+            return new CombinationBankChunk();
+        }
+        if ("DKT1".equalsIgnoreCase(_chunkId)) {
+            return null;
+        }
+        if ("DBK1".equalsIgnoreCase(_chunkId)) {
+            return new DrumkitBankChunk();
+        }
+        if ("ARP1".equalsIgnoreCase(_chunkId)) {
+            return null;
+        }
+        if ("ABK1".equalsIgnoreCase(_chunkId)) {
+            return new ArpeggioBankChunk();
+        }
+        if ("CSM1".equalsIgnoreCase(_chunkId)) {
+            return new ChecksumChunk();
+        }
+        if ("DIV1".equalsIgnoreCase(_chunkId)) {
+            return new DividedChunk();
+        }
+        if ("INI1".equalsIgnoreCase(_chunkId)) {
+            return new IniChunk();
+        }
+        if ("GLB1".equalsIgnoreCase(_chunkId)) {
+            return new GlobalChunk();
+        }
+        throw new BadDataException("Unkown chunk type - " + _chunkId + ".");
     }
     
-    public String toString () {
-        return "Chunk ID: " + _chunkId + "\n"
-             + "Size:     " + _size + "\n";
+    public String toString() {
+        return "Chunk ID: " + _chunkId + "\n" + "Size:     " + _size + "\n";
     }
     
-    public int getSize () {
+    public int getSize() {
         return _size;
     }
     
-    public String getChunkId () {
+    public String getChunkId() {
         return _chunkId;
     }
     
-    private String _chunkId;  // [4byte]
-    private int    _size;     // [4byte] (Programs/Combinations...Global)
+    private String _chunkId; // [4byte]
+    private int _size; // [4byte] (Programs/Combinations...Global)
     
     public static int BYTE_SIZE = 8;
 }
